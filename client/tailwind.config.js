83b4eaf554fa0bd4cf370e720d18903c6f34da74
RajKalyanT <trajkalyan@gmail.com>
2024-07-28 09:38:29 +0530

    added Navbar

---
+++ client/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["index.html","./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    fontFamily: {
+      sans:["Montserrat","sans-serif"]
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
