c23f8db30eac6ae03ddbfdb88e51f9c520100d0f
Ponraaj <ponraajvetrivel@gmail.com>
2024-07-29 13:33:24 +0530

    "Rough ai api setup"

--- server/index.js
+++ server/index.js
@@ -1,12 +1,16 @@
 import express from "express";  
 import dotenv from "dotenv";
 import cors from "cors"
+import userRouter from "./routes/userRoutes.js";
 
 const app = express()
 dotenv.config()
 
 const PORT = process.env.PORT
+app.use(express.json())
 app.use(cors())   
+
+app.use('/',userRouter)
 
 app.listen(PORT,()=>{
     console.log(`Server running on http://localhost:${PORT}`)
