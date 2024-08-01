dfcb3f1f94a5ec759fd5a12482e246bc0258083c
Ponraaj <ponraajvetrivel@gmail.com>
2024-08-01 20:23:41 +0530

    "fix: Minor change"

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,7 +12,7 @@ import LandingPage from './components/pages/LandingPage';
 import PageNotFound from './components/pages/PageNotFound';
 import { ToastContainer } from 'react-toastify';
 
-axios.defaults.baseURL ="http://localhost:6969"
+axios.defaults.baseURL = import.meta.env.BASE_URL
 const App = () => {
   return (
     
