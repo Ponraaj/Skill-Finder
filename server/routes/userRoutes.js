dcba42b66eca08ed86233a788fc5088ce0c3ca7e
Ponraaj <ponraajvetrivel@gmail.com>
2024-08-01 21:13:00 +0530

    added test route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,6 +4,11 @@ import generateContent from "../controllers/aiController.js"
 const userRouter = express.Router()
 
 userRouter.post('/sendmessage',generateContent)
+userRouter.get('/',(req,res)=>{
+    res.status(200).send(
+        "Server Running"
+    )
+})
 
 
 export default userRouter
